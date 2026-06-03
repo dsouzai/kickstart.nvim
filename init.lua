7b05b3fecf4f66055a02f3c5635af9ca14b0c532
Irwin D'Souza <dsouzai.gh@gmail.com>
2026-06-03 00:33:23 -0400

    Add vim.opt.termguicolors
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- init.lua
+++ init.lua
@@ -15,6 +15,8 @@ vim.g.loaded_python3_provider = 0
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+vim.opt.termguicolors = true
+
 require 'options'
 require 'mappings'
 require 'autocmds'
